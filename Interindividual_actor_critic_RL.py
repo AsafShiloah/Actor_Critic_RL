59bbf70510bc8eb77dacf4381fe027c0543e375e
asafshiloah <asaf.shiloah@mail.huji.ac.il>
2024-08-12 14:56:21 +0300

    metrics update

--- Interindividual_actor_critic_RL.py
+++ Interindividual_actor_critic_RL.py
@@ -252,10 +252,6 @@ def calculate_policy_mean(policy_all, group_assignment, HP):
 def calculate_policy_variance(policy_all, group_assignment, HP):
     group1_policy_variance = np.var(policy_all[:, :, group_assignment == 0, 0], axis=-1).flatten()
     group2_policy_variance = np.var(policy_all[:, :, group_assignment == 1, 1], axis=-1).flatten()
-    # print(policy_all)
-    print(policy_all[:, :, group_assignment == 0, 0])
-    print("hahggah")
-    print(policy_all[:, :, group_assignment == 1, 1])
     return group1_policy_variance, group2_policy_variance
 
 
