4bccabab14df5764a870e543b400affe6e703cfd
asafshiloah <asaf.shiloah@mail.huji.ac.il>
2024-08-12 14:33:24 +0300

    metrics update

--- Interindividual_actor_critic_RL.py
+++ Interindividual_actor_critic_RL.py
@@ -250,8 +250,8 @@ def calculate_policy_mean(policy_all, group_assignment, HP):
 
 
 def calculate_policy_variance(policy_all, group_assignment, HP):
-    group1_policy_variance = np.var(policy_all[:, :, group_assignment == 0, 0], axis=1).flatten()
-    group2_policy_variance = np.var(policy_all[:, :, group_assignment == 1, 1], axis=1).flatten()
+    group1_policy_variance = np.var(policy_all[:, :, group_assignment == 0, 0], axis=0).flatten()
+    group2_policy_variance = np.var(policy_all[:, :, group_assignment == 1, 1], axis=0).flatten()
     return group1_policy_variance, group2_policy_variance
 
 
@@ -270,8 +270,8 @@ def calculate_metrics_over_time(HP):
     group1_policy_variance, group2_policy_variance = calculate_policy_variance(policy_all, group_assignment, HP)
     reward_mean = calculate_reward_mean(V_all, HP)
 
-    group1_value_mean = np.mean(V_all[:, :, group_assignment == 0], axis=2).flatten()
-    group2_value_mean = np.mean(V_all[:, :, group_assignment == 1], axis=2).flatten()
+    group1_value_mean = np.mean(V_all[:, :, group_assignment == 0], axis=0).flatten()
+    group2_value_mean = np.mean(V_all[:, :, group_assignment == 1], axis=0).flatten()
 
     return group1_policy_mean, group2_policy_mean, group1_policy_variance, group2_policy_variance, reward_mean, group1_value_mean, group2_value_mean
 
