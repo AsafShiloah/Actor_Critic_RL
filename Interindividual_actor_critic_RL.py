903b8b49e4f228c99b7cfb995d00082a8ca2f2c4
asafshiloah <asaf.shiloah@mail.huji.ac.il>
2024-08-13 11:15:03 +0300

    metrics update

--- Interindividual_actor_critic_RL.py
+++ Interindividual_actor_critic_RL.py
@@ -338,7 +338,7 @@ def mean_plot_with_time(group1_policy_mean, group2_policy_mean,
     axs[0, 1].grid(True)
 
     # Plot reward means by group
-    axs[1, 0].plot(rounds, reward_mean, label='Group 1', color='b', linewidth=1)
+    axs[1, 0].plot(rounds, reward_mean, label='mean reward', color='b', linewidth=1)
     axs[1, 0].set_title('Reward Mean Over Time')
     axs[1, 0].set_xlabel('Rounds')
     axs[1, 0].set_ylabel('Reward Mean')
